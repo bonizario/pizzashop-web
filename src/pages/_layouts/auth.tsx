6ddd15aff6ca2abcf620d6f4e71d57436921fa49
Gabriel Bonizário <gabrielbonizario98@gmail.com>
2023-12-26 18:17:19 -0300

    feat: add orders page

--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom';
 
 export function AuthLayout() {
   return (
-    <div className="grid min-h-screen grid-cols-2">
+    <main className="grid min-h-screen grid-cols-2">
       <div className="flex h-full flex-col justify-between border-r border-foreground/5 bg-muted p-10 text-muted-foreground">
         <div className="flex items-center gap-3 text-lg text-foreground">
           <Pizza className="h-5 w-5" />
@@ -18,6 +18,6 @@ export function AuthLayout() {
       <div className="relative flex flex-col items-center justify-center">
         <Outlet />
       </div>
-    </div>
+    </main>
   );
 }
